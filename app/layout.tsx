fea8174349135d810905194cf8d5eb9109b20cfe
Jayeshpatil9869 <jayeshbpatil8830@gmail.com>
2026-03-17 15:13:06 +0530

    feat: Create root layout for the Next.js application, including metadata, fonts, global styles, structured data, and essential UI components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2ZWTLN58G2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2ZWTLN58G2');
+            `
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
